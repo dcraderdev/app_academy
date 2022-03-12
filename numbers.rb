c7f64be228163530e0cd29b57b686d0d343a0ff4
donovan crader <dcraderdev@gmail.com>
2022-03-11 18:00:24 -0800

    updated notes

--- numbers.rb
+++ numbers.rb
@@ -105,7 +105,13 @@ def average_of_three(num1, num2, num3)
   puts average_of_three(2, 8, 1)   # => 3.666666
 
 
+  def goodbye(name)
+	return "Hey, " + name + ", goodbye my friend."
+end
 
+puts goodbye("Daniel")   # => "Bye Daniel."
+puts goodbye("Mark")     # => "Bye Mark."
+puts goodbye("Beyonce")  # => "Bye Beyonce."
 
 
 
