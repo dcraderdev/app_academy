3ed381fafb8d442cf5f348bea98a51c97e1653ee
donovan crader <dcraderdev@gmail.com>
2022-03-12 01:26:00 -0800

    notes notes notes

--- numbers.rb
+++ numbers.rb
@@ -114,4 +114,147 @@ puts goodbye("Mark")     # => "Bye Mark."
 puts goodbye("Beyonce")  # => "Bye Beyonce."
 
 
+def is_div_by_5(number)
+    return true if number % 5 == 0 else return false
+end
+    
+    puts is_div_by_5(10) # => true
+    puts is_div_by_5(40) # => true
+    puts is_div_by_5(42) # => false
+    puts is_div_by_5(8)  # => false
+
+
+def either_only(number)
+    return true if (number % 3 == 0 || number % 5 == 0) && !(number % 3 == 0 && number % 5 == 0)
+    else return false
+  end
+  
+  puts either_only(9)  # => true
+  puts either_only(20) # => true
+  puts either_only(7)  # => false
+  puts either_only(15) # => false
+  puts either_only(30) # => false
+
+  def larger_number(num1, num2)
+    return num1 if num1 > num2 
+    else return num2
+end
+    
+puts larger_number(42, 28)   # => 42
+puts larger_number(99, 100)  # => 100
+
+
+def longer_string(str1, str2)
+    if str1.length > str2.length
+     return str1
+     else 
+     return str2
+     end
+   end
+   
+   
+   puts longer_string("app", "academy") # => "academy"
+   puts longer_string("summer", "fall") # => "summer"
+   puts longer_string("hello", "world") # => "hello"
+
+
+   def number_check(num)
+    if num > 0 
+      return "positive"
+    elsif num == 0
+      return "zero"
+    else
+      return "negative"
+    end
+      
+    end
+    
+    puts number_check(5)    # => "positive"
+    puts number_check(-2)   # => "negative"
+    puts number_check(0)    # => "zero"
+
+
+def word_check(word)
+    if word.length > 6
+      return "long"
+    elsif word.length < 6
+      return "short"
+    else 
+      return "medium"
+    end
+      
+    end
+    
+puts word_check("contraption") # => "long"
+puts word_check("fruit")       # => "short"
+puts word_check("puzzle")      # => "medium"
+
+
+# iterating through strings with loops
+
+str = "hello"
+str.downcase
+
+def count_e(word)
+  
+	count = 0
+    i = 0
+  while i <= word.length
+    
+    if word[i] == "e"
+      count += 1
+    end
+      
+    i += 1
+  end
+  return count
+end
+
+puts count_e("movie") # => 1
+puts count_e("excellent") # => 3
+
+
+def count_a(word)
+    count = 0
+      i = 0
+      while i < word.length
+        if word.downcase[i] == "a"
+          count += 1
+        end
+        
+          i += 1
+      end
+      return count
+        
+    end
+    
+    puts count_a("application")  # => 2
+    puts count_a("bike")         # => 0
+    puts count_a("Arthur")       # => 1
+    puts count_a("Aardvark")     # => 3
+
+
+def sum_nums(max)
+    count = 0
+      i = 1
+      while i <= max
+        count += i
+          i += 1
+      end
+      
+    return count
+        
+    end
+    
+    puts sum_nums(4) # => 10, because 1 + 2 + 3 + 4 = 10
+    puts sum_nums(5) # => 15
+
+
+
+        
+
+
+
+
+
 
