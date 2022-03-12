1f69c441095b51e34ea0abcb98716b102fb974ac
donovan crader <dcraderdev@gmail.com>
2022-03-11 16:32:27 -0800

    more notes

--- numbers.rb
+++ numbers.rb
@@ -9,7 +9,52 @@ puts 2.5 + 0.5
 
 puts 13 % 5
 # 3
-# returns three because it is not a floating point class so it rounds down. it works out to 13/5 = 2 r 3
+# returns three because it is not a floating point class so it returns the remainder. it works out to 13/5 = 2 r 3
 puts 14 % 5
 # 4
-# returns three because it is not a floating point class so it rounds up. it works out to 13/5 = 2 r 4
+# returns three because it is not a floating point class and it returns the remainder. it works out to 14/5 = 2 r 4
+
+Comparisons review -- returns a true or false -- BOOLEAN --
+
+equal ==
+not equal !=
+greater than >
+less than < 
+greater than or equal >=
+less than or equal <= 
+
+
+puts 27 % 6
+
+
+
+Boolean data type structure review
+
+# operations
+# ! (not)
+# && (and)
+# || (or)
+
+puts false && false
+puts false && true
+puts true && false
+puts true && true
+
+puts false || false
+puts false || true
+puts true || false
+puts true || true
+
+puts false == false
+puts false == true
+puts true == false
+puts true == true
+
+puts false != false
+puts false != true
+puts true != false
+puts true != true
+
+
+
+
