6f99590e62c7e51c7b96acc545926a66c30c6c55
donovan crader <dcraderdev@gmail.com>
2022-03-15 20:25:20 -0700

    note note note

--- numbers.rb
+++ numbers.rb
@@ -674,3 +674,44 @@ puts is_valid_name("Robert Downey Jr") # => true
 puts is_valid_name("ROBERT DOWNEY JR") # => false
 puts is_valid_name("Robert") # => true for my test, false otherwise
 
+
+
+
+
+
+split into 2 parts at @ 
+
+# For simplicity, we'll consider an email valid when it satisfies all of the following:
+# - contains exactly one @ symbol
+# - contains only lowercase alphabetic letters before the @
+# - contains exactly one . after the @
+
+def is_valid_email(str)
+  parts = str.split("@")
+    if parts.length != 2
+      return false
+    end
+
+  first = parts[0]
+  second = parts[1]
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  first.each_char do |char|
+      if !alpha.include?(char)
+        return false
+      end
+  end
+
+  if second.split(".").length == 2
+    return true 
+  else
+    return false
+  end
+
+end
+
+puts is_valid_email("abc@xy.z")         # => true
+puts is_valid_email("jdoe@gmail.com")   # => true
+puts is_valid_email("jdoe@g@mail.com")  # => false
+puts is_valid_email("jdoe42@gmail.com") # => false
+puts is_valid_email("jdoegmail.com")    # => false
+puts is_valid_email("az@email")         # => false
