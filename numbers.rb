77b8ad83c30413aff3e0e89d922cc0e425aa5398
donovan crader <dcraderdev@gmail.com>
2022-03-17 14:00:24 -0700

    array_translate

--- numbers.rb
+++ numbers.rb
@@ -859,3 +859,24 @@ arr_2 = [
 ]
 
 print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+
+
+
+
+# Write a method array_translate that takes in an array whose elements alternate between words and numbers. 
+# The method should return a string where each word is repeated the number of times that immediately follows in the array.
+
+def array_translate(array)
+  new_str = ""
+
+  	
+end
+
+
+print array_translate(["Cat", 2, "Dog", 3, "Mouse", 1]); # => "CatCatDogDogDogMouse"
+puts
+
+print array_translate(["red", 3, "blue", 1]); # => "redredredblue"
+puts
+
+
