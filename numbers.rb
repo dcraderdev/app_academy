f67aba20415d1cf0353a7517d1a38ecbec601380
donovan crader <dcraderdev@gmail.com>
2022-03-16 19:52:12 -0700

    uniq combos

--- numbers.rb
+++ numbers.rb
@@ -757,3 +757,25 @@ puts
 
 
 # nested loops
+
+Write a method combinations that takes in an array of unique elements, the method should return a 2D array representing all 
+possible combinations of 2 elements of the array.
+
+
+def combinations(arr)
+	array = []
+  arr.each_with_index do |char1, idx1|
+    arr.each_with_index do |char2, idx2|
+      if idx2 > idx1
+        array << [char1, char2]
+      end
+    end
+  end
+  return array
+end
+
+print combinations(["a", "b", "c"]); # => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ]
+puts
+
+print combinations([0, 1, 2, 3]); # => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ]
+puts
