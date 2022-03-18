f1a941053640c5cf3debee6d04518dad61da6a00
donovan crader <dcraderdev@gmail.com>
2022-03-18 12:55:43 -0700

    # Map By Key

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -80,5 +80,30 @@ p map_by_name(countries) #=> ["Japan", "Hungary", "Kenya"]
 
 
 
+# Map By Key
+# Write a method map_by_key that takes in an array of hashes and a key string. 
+# The method should returns a new array containing the values from each hash for the given key.
+
+iterate over arr
+check what key we are looking for
+return keys values into new array
+
+
+def map_by_key(arr, key)
+  return arr.map { |hash| hash[key] }
+end
+
+locations = [
+  {"city"=>"New York City", "state"=>"New York", "coast"=>"East"},
+  {"city"=>"San Francisco", "state"=>"California", "coast"=>"West"},
+  {"city"=>"Portland", "state"=>"Oregon", "coast"=>"West"},
+]
+
+print map_by_key(locations, "state") #=> ["New York", "California", "Oregon"]
+puts
+print map_by_key(locations, "city") #=> ["New York City", "San Francisco", "Portland"]
+puts
+
+
 
 
