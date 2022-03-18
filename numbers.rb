a2a914dd624635e2f5aa5cbb8885f99d9f0c7f2b
donovan crader <dcraderdev@gmail.com>
2022-03-17 17:47:42 -0700

    # Word Lengths
    # Word Lengths

--- numbers.rb
+++ numbers.rb
@@ -1030,5 +1030,50 @@ def get_double_age(hash)
   return hash["age"] * 2
 end
 
+
 puts get_double_age({"name"=>"App Academy", "age"=>5}) # => 10
 puts get_double_age({"name"=>"Ruby", "age"=>23})       # => 46
+
+
+# Get Full Name
+# Write a method get_full_name that takes in a hash containing a first, last, and title. 
+# The method should return a string representing the hash's full name
+
+
+def get_full_name(hash)
+return hash["first"] + " " + hash["last"] + ", the " + hash["title"] + "."
+end
+
+hash1 = {"first"=>"Michael", "last"=>"Jordan", "title"=> "GOAT"}
+puts get_full_name(hash1) # => "Michael Jordan, the GOAT"
+
+hash2 = {"first"=>"Fido", "last"=>"McDog", "title"=> "Loyal"}
+puts get_full_name(hash2) # => "Fido McDog, the Loyal"
+
+
+# Word Lengths
+# Write a method word_lengths that takes in a sentence string 
+# and returns a hash where every key is a word of the sentence, 
+# and its' corresponding value is the length of that word.
+
+
+
+# split sentence into words
+# set keys as words
+# set value as length of word
+
+def word_lengths(sentence)
+  word_length = Hash.new(0)
+  words = sentence.split(" ")
+
+  words.each do |word|
+    word_length[word] = word.length
+   # word[value] = word.length
+
+  end
+  return word_length
+end
+
+puts word_lengths("this is fun") #=> {"this"=>4, "is"=>2, "fun"=>3}
+puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt,"=>6, "leave"=>5, "it"=>2, "out"=>3}
+
