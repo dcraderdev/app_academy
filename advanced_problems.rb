8217e66a937089eb6f8df361ccf42778e4f9df3f
donovan crader <dcraderdev@gmail.com>
2022-03-18 17:01:40 -0700

    # Prime Factors
    what a doozy

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -398,7 +398,6 @@ def prime?(num)
       return false
     end
   end
-
     return true
 end
 
@@ -407,6 +406,55 @@ end
 p pick_primes([2, 3, 4, 5, 6]) #=> [2, 3, 5]
 
 p pick_primes([101, 20, 103, 2017]) #=> [101, 103, 2017]
+
+
+
+
+# Prime Factors
+# Write a method prime_factors that takes in a number and returns an array 
+# containing all of the prime factors of the given number.
+
+
+def prime_factors(num)
+    arr = []
+    prime_factors = []
+
+  (1..num).each do |l|
+      if num % l == 0
+        arr << l
+      end
+  end
+
+  arr.map { |k| prime_factors << k if prime?(k) == true}
+
+  return prime_factors
+end
+
+def prime?(num)
+  if num < 2
+    return false
+  end
+
+  (2...num).each do |n|
+    if num % n == 0
+      return false
+    end
+  end
+    return true
+end
+
+print prime_factors(24) #=> [2, 3]
+puts
+print prime_factors(60) #=> [2, 3, 5]
+puts
+
+
+
+
+
+
+
+
 
 
 
