1b600b73a2e31ad8b521026383a82484072a4247
donovan crader <dcraderdev@gmail.com>
2022-03-18 21:05:19 -0700

    # Triple Sequence

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -509,7 +509,7 @@ def perfect_square?(num)
       return true
     end
   end
-  
+
   return false
 end
 
@@ -522,3 +522,47 @@ puts perfect_square?(9)   #=> true
 puts perfect_square?(25)  #=> true
 
 
+# Triple Sequence
+# Write a method triple_sequence that takes in two numbers, start and length. 
+# The method should return an array representing a sequence that begins with start 
+# and is length elements long. In the sequence, every element should be 3 times the 
+# previous element. Assume that the length is at least 1.
+
+
+
+
+start with original element
+take last element and * 3
+then take that last ele and * 3
+
+def triple_sequence(start, length)
+  new_arr = []
+  i = 0
+   new_arr << start * 3
+
+  i += 1
+
+end
+
+
+
+print triple_sequence(2, 4) # => [2, 6, 18, 54]
+puts
+print triple_sequence(4, 5) # => [4, 12, 36, 108, 324]
+puts
+
+
+def triple_sequence(start, length)
+  seq = [start]
+
+  while seq.length < length
+    seq << seq[-1] * 3
+  end
+
+  return seq
+end
+
+print triple_sequence(2, 4) # => [2, 6, 18, 54]
+puts
+print triple_sequence(4, 5) # => [4, 12, 36, 108, 324]
+puts
