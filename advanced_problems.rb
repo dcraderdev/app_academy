2ab8393fc1f191f00e5d4a13a9566e1ab8c65b34
donovan crader <dcraderdev@gmail.com>
2022-03-21 19:53:34 -0700

    # Same Char Collapse

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -1072,3 +1072,49 @@ end
 
 puts consonant_cancel("down the rabbit hole") #=> "own e abbit ole"
 puts consonant_cancel("writing code is challenging") #=> "iting ode is allenging"
+
+
+# Same Char Collapse
+# Write a method same_char_collapse that takes in a string and returns a collapsed version 
+# of the string. To collapse the string, we repeatedly delete 2 adjacent characters that are 
+# the same until there are no such adjacent characters. If there are multiple pairs that can 
+# be collapsed, delete the leftmost pair. For example, we take the following steps to collapse 
+# "zzzxaaxy": zzzxaaxy -> zxaaxy -> zxxy -> zy
+
+takes in str
+deletes first repeated characters and condenses str
+then checks str again for repeated char
+checking each time if theres any adjacent same charatcers and deleting if so
+
+
+
+def same_char_collapse(str)
+  reducible = true
+
+  while reducible
+    chars = str.split("")
+    reducible = false
+
+    chars.each.with_index do |char, i|
+      if chars[i] == chars[i + 1]
+        chars[i] = ""
+        chars[i + 1] = ""
+        reducible = true
+      end
+    end
+
+    str = chars.join("")
+  end
+
+  return str
+end
+
+puts same_char_collapse("zzzxaaxy")   #=> "zy"
+# because zzzxaaxy -> zxaaxy -> zxxy -> zy
+
+
+puts same_char_collapse("zzzxaaxy")   #=> "zy"
+# because zzzxaaxy -> zxaaxy -> zxxy -> zy
+
+puts same_char_collapse("uqrssrqvtt") #=> "uv"
+# because uqrssrqvtt -> uqrrqvtt -> uqqvtt -> uvtt -> uv
