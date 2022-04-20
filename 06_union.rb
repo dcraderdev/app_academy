74be5479cac77d23ebf4607f131a3aff1aca884f
donovan crader <dcraderdev@gmail.com>
2022-04-20 12:11:15 -1000

    #06_union
    #06_union

--- 06_union.rb
+++ 06_union.rb
@@ -1,6 +1,38 @@
+#06_union
+
+
 # Write a method, union, that accepts any number of arrays as arguments.
 # The method should return an array containing all elements of the given arrays.
 
 
 p union(["a", "b"], [1, 2, 3]) # => ["a", "b", 1, 2, 3]
 p union(["x", "y"], [true, false], [20, 21, 23]) # => ["x", "y", true, false, 20, 21, 23]
+
+
+def union(*arr)
+  union = []
+  arr.map { |el| el.map {|l| union << l } }
+  return union
+end
+
+  p union(["a", "b"], [1, 2, 3]) 
+  # => ["a", "b", 1, 2, 3]
+
+p union(["x", "y"], [true, false], [20, 21, 23]) 
+# => ["x", "y", true, false, 20, 21, 23]
+
+
+
+
+# # better answer
+
+
+def union(*arr)
+  arr.inject { |acc, el|  acc + el }
+end
+
+p union(["a", "b"], [1, 2, 3]) 
+# => ["a", "b", 1, 2, 3]
+
+p union(["x", "y"], [true, false], [20, 21, 23]) 
+# => ["x", "y", true, false, 20, 21, 23]
