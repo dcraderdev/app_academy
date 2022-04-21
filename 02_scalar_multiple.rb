277efdfd20d0435dc920f64a2284e743851e2e39
donovan crader <dcraderdev@gmail.com>
2022-04-21 09:21:06 -1000

    def scalar_multiple!
    def scalar_multiple!

--- 02_scalar_multiple.rb
+++ 02_scalar_multiple.rb
@@ -1,3 +1,6 @@
+# # def scalar_multiple!
+
+
 # Write a method, scalar_multiple!(array, multiplier), that accepts an array and a number as args. 
 # The method should mutate the given array by multiplying all of the elements by the multiplier.
 # The method should return the given array.
@@ -6,9 +9,18 @@
 # to the object_id of the returned array. The exact object_ids you get back don't matter. We just want the ids
 # to be the same before and after calling your method.
 
-def scalar_multiple!(array, multiplier)
 
+
+
+# def scalar_multiple!(array, multiplier)
+#   array.collect! { |n| n * multiplier}
+# end
+
+
+def scalar_multiple!(array, multiplier)
+  array.map! { |n| n * multiplier}
 end
+
 
 
 array_1 = [7, 0, 4]
@@ -17,7 +29,7 @@ result_1 = scalar_multiple!(array_1, 3)
 p result_1                                      # => [21, 0, 12]
 p result_1.object_id                            # => 70326264380840
 
-
+puts"---"
 array_2 = [90, 30, 4, 12]
 p array_2.object_id                             # => 70326264378100
 result_2 = scalar_multiple!(array_2, 0.5)
