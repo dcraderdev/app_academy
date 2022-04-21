47554a2b17e1f5d5514be3d41f1b3e8fd6cac105
donovan crader <dcraderdev@gmail.com>
2022-04-20 15:34:26 -1000

    # compressed string
    # compressed string

--- 05_compress_str.rb
+++ 05_compress_str.rb
@@ -1,6 +1,36 @@
+# compressed string
+
+
 # Write a method, compress_str(str), that accepts a string as an arg.
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
+
+
+
+def compress_str(str)
+  compressed = ""
+  
+  
+  i = 0
+  while i < str.length
+    char = str[i]
+
+    count = 0
+    while char == str[i]
+      count += 1
+      i += 1
+    end
+
+    if count > 1
+      compressed += count.to_s + char
+    else
+      compressed += char
+    end
+  end
+
+  compressed
+end
+
 
 
 p compress_str("aaabbc")        # => "3a2bc"
