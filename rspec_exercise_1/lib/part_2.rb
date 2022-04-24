92c643ce3e70ddf8ac014d2041603898e1501450
donovan crader <dcraderdev@gmail.com>
2022-04-24 10:15:25 -1000

    def hipsterfy(word)
    def hipsterfy(word)

---
+++ rspec_exercise_1/lib/part_2.rb
@@ -0,0 +1,39 @@
+# Part 2:
+#   hipsterfy
+#     should accept a word as an arg
+#     should return the word with it's last vowel (a, e, i, o, u) removed
+#     should return the original word if it contains no vowels
+#   vowel_counts
+#     should accept string as an arg
+#     should return a hash where each key is a vowel of string and it's value is the number of times it appears
+#   caesar_cipher
+#     should accept a message and a number (n) as an arg
+#     should return a new string where every letter of the message is shifted n positions in the alphabet
+#     should not modify non-alphabetic characters
+
+
+def hipsterfy(word)
+  vowels = "aeiouAEIOU"
+
+  i = word.length - 1
+  while i >= 0
+    if vowels.include?(word[i])
+      return word[0...i] + word[i+1..-1]
+    end
+
+    i -= 1
+  end
+  return word
+end
+
+
+
+def vowel_counts(str)
+
+end
+
+
+
+def caesar_cipher(str, n)
+
+end
